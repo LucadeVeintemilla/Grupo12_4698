36c39fa67e47258a402498e7646d5657f19e34a5
Kevin Vargas <70618510+KernyMC@users.noreply.github.com>
2022-06-02 22:40:49 -0500

    Update Lista.h

--- ListaDoble/Lista.h
+++ ListaDoble/Lista.h
@@ -1,4 +1,14 @@
-
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    Luca Manuel de Veintemilla - lmde2@espe.edu.ec
+				Kevin Alexander Vargas - kavargas7@espe.edu.ec
+					
+	FECHA DE CREACION:        02-06-2022 	
+	FECHA DE MODIFICACION:    02-06-2022
+	Grupo #12
+*/
 #include <iostream>
 #include <stdio.h>  
 #include <stdlib.h>  
