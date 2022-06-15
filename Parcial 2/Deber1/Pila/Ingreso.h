f24a1f6b27c4f2c1482606606956e7ebc1e14d97
Kevin Vargas <70618510+KernyMC@users.noreply.github.com>
2022-06-14 20:19:40 -0500

    Update Ingreso.h

--- Parcial 2/Deber1/Pila/Ingreso.h
+++ Parcial 2/Deber1/Pila/Ingreso.h
@@ -1,3 +1,14 @@
+/*	UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+	DEPARTAMENTO DE CIENCIAS DE LA COMPUTACION
+    INGENIERA DE SOFTWARE
+                
+	AUTORES:    Luca Manuel de Veintemilla - lmde2@espe.edu.ec
+				Kevin Alexander Vargas - kavargas7@espe.edu.ec
+					
+	FECHA DE CREACION:        14-06-2022 	
+	FECHA DE MODIFICACION:    14-06-2022
+	Grupo #12
+*/
 #include <iostream>
 #include "Validacion.h"
 
